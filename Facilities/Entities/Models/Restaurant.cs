bf7482c4ee9855442ad80cf98016c3040fcf7bd9
Kirill Vusov <kvusov@bk.ru>
2025-07-21 12:13:02 +0300

    Initial setup

---
+++ Facilities/Entities/Models/Restaurant.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    class Restaurant : Facility
+    {
+        public string? CuisineType { get; set; } 
+        public int SeatingCapacity { get; set; } 
+        public bool HasDeliveryService { get; set; } 
+        public bool HasTakeoutService { get; set; } 
+    }
+}
