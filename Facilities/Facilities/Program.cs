d084260e3bf0b2eb88fb989373569d98a047e4b9
Kirill Vusov <kvusov@bk.ru>
2025-07-22 11:27:59 +0300

    Added validation.

--- Facilities/Facilities/Program.cs
+++ Facilities/Facilities/Program.cs
@@ -2,6 +2,8 @@ using Application.Contracts;
 using Facilities.Extensions;
 using Infrastructure;
 using Application;
+using FluentValidation.AspNetCore;
+using Application.Validators;
 using FluentValidation;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,8 +13,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.ConfigureDB(builder.Configuration);
 builder.Services.AddAutoMapper(typeof(Application.AssemblyReference).Assembly);
 builder.Services.AddControllers();
+
 builder.Services.AddScoped<IFacilityRepository, FacilitiesRepository>();
 builder.Services.AddScoped<IFacilityService, FacilitiesService>();
+
+builder.Services.AddFluentValidationAutoValidation();
+builder.Services.AddFluentValidationClientsideAdapters();
+builder.Services.AddValidatorsFromAssemblyContaining<FacilityDtoValidator>();
 
 var app = builder.Build();
 
