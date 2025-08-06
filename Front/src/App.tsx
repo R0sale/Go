f54134ede066927ef5a3cb93ac4a31a58361ced8
Kirill Vusov <kvusov@bk.ru>
2025-08-06 13:15:30 +0300

    Added logIn and reworked signIn pages.

--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -1,12 +1,14 @@
 import MainPage from "./pages/MainPage";
-import SignInPage from "./pages/SignInPage";
+import SignUpPage from "./pages/SignUpPage";
+import LogInPage from "./pages/LoginPage";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
       <Routes>
         <Route path="/" element={<MainPage />} />
-        <Route path="/login" element={<SignInPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LogInPage />} />
       </Routes>
   );
 }
--- Front/src/pages/SignUpPage.tsx
+++ Front/src/pages/SignUpPage.tsx
@@ -1,21 +1,29 @@
 import React, { useState } from "react";
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase";
 import bgImage from '../assets/worldmap.jpg';
+import { useNavigate } from "react-router-dom";
 
-const SignInPage: React.FC = () => {
+const SignUpPage: React.FC = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [repeatedPassword, setRepeatedPassword] = useState('');
+    const navigate = useNavigate();
+
+    const goToLogIn = () => {
+                navigate('/login');
+            };
 
     const login = async () => {
         try {
-            const userCredentials = await signInWithEmailAndPassword(auth, email, password);
+            const userCredentials = await createUserWithEmailAndPassword(auth, email, password);
             const token = await userCredentials.user.getIdToken();
 
             const response = await fetch('', {
                 method: 'POST',
                 headers: {
-                    'Authorization': `Bearer ${token}`
+                    'Authorization': `Bearer ${token}`,
+                    'Content-Type': 'application/json'
                 },
             });
 
@@ -37,22 +45,22 @@ const SignInPage: React.FC = () => {
             <div className="h-screen w-1/3 bg-white m-auto">
                 <div>
                     <label className="block m-2 font-semibold">Email</label>
-                    <input placeholder="Enter your email" type="email" value={email} className="w-inp border-2 p-1 rounded-md border-gray-300 h-10 m-2" onChange={(e) => {setEmail(e.target.value)}}></input>
+                    <input placeholder="Enter your email" type="email" value={email} className="w-full border-2 p-1 rounded-md border-gray-300 h-10 m-2" onChange={(e) => {setEmail(e.target.value)}}></input>
 
                     <label className="block m-2 font-semibold">Password</label>
-                    <input placeholder="Enter your password" type="password" value={password} className="w-inp p-1 border-2 rounded-md border-gray-300 h-10 m-2" onChange={(e) => {setPassword(e.target.value)}}></input>
+                    <input placeholder="Enter your password" type="password" value={password} className="w-full p-1 border-2 rounded-md border-gray-300 h-10 m-2" onChange={(e) => {setPassword(e.target.value)}}></input>
 
                     <label className="block m-2 font-semibold">Repeat password</label>
-                    <input placeholder="Repeat your password" type="password" value={password} className="w-inp p-1 border-2 rounded-md border-gray-300 h-10 m-2" onChange={(e) => {setPassword(e.target.value)}}></input>
+                    <input placeholder="Repeat your password" type="password" value={repeatedPassword} className="w-full p-1 border-2 rounded-md border-gray-300 h-10 m-2" onChange={(e) => {setRepeatedPassword(e.target.value)}}></input>
 
                     <button className="m-2 w-24 border-2 border-gray-300" onClick={login}>Sign In</button>
                 </div>
                 <p className="font-bold m-2 text-right">Already have an account?</p>
                 <div className="flex justify-end m-2 mt-4">
-                    <button className="w-24 border-2 border-gray-300">Log In</button>
+                    <button className="w-24 border-2 border-gray-300" onClick={goToLogIn}>Log In</button>
                 </div>
             </div>
         </div>);
 }
 
-export default SignInPage;
+export default SignUpPage;
