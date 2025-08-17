134da13889c54544f6f0a21ae3e05e37eefb679e
Kirill Vusov <kvusov@bk.ru>
2025-08-17 16:59:14 +0300

    Added models.

---
+++ Front/src/models/MyFacility.ts
@@ -0,0 +1,22 @@
+export type MyFacility = {
+    id: string,
+    name: string,
+    coordinates: {
+        longitude: number,
+        latitude: number
+    },
+    phoneNumber: string,
+    email: string,
+    description: string,
+    websiteURL: string,
+    schedule: {
+        [key: string]: string;
+        Monday: string,
+        Tuesday: string,
+        Wednesday: string,
+        Thursday: string,
+        Friday: string,
+        Saturday: string,
+        Sunday: string
+    }
+}
