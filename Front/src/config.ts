ce708b9355fcf1b9db29845cb2582b13841a2fad
Kirill Vusov <kvusov@bk.ru>
2025-09-09 15:09:40 +0300

    Corrected irl in config.

--- Front/src/config.ts
+++ Front/src/config.ts
@@ -7,6 +7,6 @@ export const config = {
     CREATE_FACILITY_URL: 'http://localhost:5000/api/facility',
     GET_TRANSPORT_URL: 'http://localhost:5002/api/transport', 
     GET_ALL_USERS_URL: 'http://localhost:5001/api/users',
-    GET_USER_BY_UID__URL: 'http://localhost:5001/api/users/uid/',
+    GET_USER_BY_UID_URL: 'http://localhost:5001/api/users/uid/',
     UPDATE_USER_ROLES_URL: 'http://localhost:5001/api/users/roles/',
 };
