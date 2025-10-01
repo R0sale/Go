dfa3a2a7dc7f740637ec5281dec84a2b4145ad07
Kirill Vusov <kvusov@bk.ru>
2025-10-01 17:14:15 +0300

    Added redis to transport service.

--- Transport/Presentation/Program.cs
+++ Transport/Presentation/Program.cs
@@ -72,6 +72,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSingleton<IRpcServer, RpcServer>();
 builder.Services.AddHostedService<RabbitMqInitializer>();
 
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration["Redis:Host"];
+    options.ConfigurationOptions = new StackExchange.Redis.ConfigurationOptions()
+    {
+        AbortOnConnectFail = true,
+        EndPoints = { options.Configuration }
+    };
+});
+
 builder.Services.ConfigureDB(builder.Configuration);
 
 builder.Services.CreateFirebaseApp(builder.Configuration);
